ea15e35d4a34f80f9e46f4eb3e14d6b9d5102603
Sam Ward-Pickering <samward-pickering@Sams-Air.hub>
2021-07-03 18:09:24 +1200

    adding tail to snake

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ console.log ('hello')
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
 
+class SnakePart{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+
+
+    } 
+
+};
+
 let speed = 7
 
 let tileCount = 20
@@ -10,6 +20,10 @@ let tileSize = canvas.width / tileCount -2
 
 let headX = 10
 let headY = 10
+
+const snakeParts = []
+
+let tailLength = 0;
 
 let xVelocity = 0 
 let yVelocity = 0 
@@ -34,6 +48,17 @@ function clearScreen(){
 function drawSnake(){
     ctx.fillStyle = 'green'
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
+
+    ctx.fillStyle = 'green' 
+    for (let i =0; i < snakeParts.length; i++){
+        let part = snakeParts[i]
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
+
+    }
+    snakeParts.push(new SnakePart(headX, headY))
+    if (snakeParts.length > tailLength){
+        snakeParts.shift()
+    }
     
 }
 
@@ -52,6 +77,8 @@ function checkAppleCollision(){
     if(appleX === headX && appleY === headY){
         appleX = Math.floor(Math.random() * tileCount)
         appleY = Math.floor(Math.random() * tileCount)
+        tailLength++
+        console.log(tailLength);
     }
 
 }
